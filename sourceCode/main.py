3ff08269ab413ee6f536d9095e3c09ebe2a22eb4
fcastelain <fcastelain@excilys.com>
2018-01-06 18:44:35 +0100

    [FEAT] - Add a basic back in python with 2 endpoint in REST GET.

---
+++ sourceCode/main.py
@@ -0,0 +1,40 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+hostport = 8081
+hostname = "127.0.0.1"
+
+# HTTPRequestHandler class
+class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
+
+    # GET
+    def do_GET(self):
+        # Send response status code
+        self.send_response(200)
+
+        if self.path == "/add":
+            message = "add 1 to iterator"
+        elif self.path == "/remove":
+            message = "remove 1 to iterator"
+        else:
+            message = "Wrong ULR"
+
+        # Send headers
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+        # Write content as utf-8 data
+        self.wfile.write(bytes(message, "utf8"))
+
+        return
+
+
+def run():
+    print('starting server...')
+
+    # Server settings
+    server_address = (hostname, hostport)
+    httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
+    print('running server...')
+    httpd.serve_forever()
+
+run()
